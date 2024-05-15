60978e1fe069cbff4742b9dc24f25712a2bf0db8
Donkie <daniel.cf.hultgren@gmail.com>
2024-05-15 19:02:31 +0200

    Updated compile filament script with latest new fields

--- scripts/compile-filaments.py
+++ scripts/compile-filaments.py
@@ -1,8 +1,58 @@
 """This script takes in the directory of filament json files, expands them and writes them to a single output json."""
 
+from enum import Enum
 import json
 from pathlib import Path
 from typing import Iterator
+from typing import TypedDict, NotRequired
+
+
+class SpoolType(Enum):
+    PLASTIC = "plastic"
+    CARDBOARD = "cardboard"
+    METAL = "metal"
+
+
+class Finish(Enum):
+    MATTE = "matte"
+    GLOSSY = "glossy"
+
+
+class MultiColorDirection(Enum):
+    COAXIAL = "coaxial"
+    LONGITUDINAL = "longitudinal"
+
+
+class Pattern(Enum):
+    MARBLE = "marble"
+    SPARKLE = "sparkle"
+
+
+class Weight(TypedDict):
+    weight: float
+    spool_weight: NotRequired[float]
+    spool_type: NotRequired[SpoolType | None]
+
+
+class Color(TypedDict):
+    name: str
+    hex: str | list[str]
+
+
+class Filament(TypedDict):
+    name: str
+    material: str
+    density: float
+    weights: list[Weight]
+    diameters: list[float]
+    colors: list[Color]
+    extruder_temp: NotRequired[int]
+    bed_temp: NotRequired[int]
+    finish: NotRequired[Finish | None]
+    multi_color_direction: NotRequired[MultiColorDirection | None]
+    pattern: NotRequired[Pattern | None]
+    translucent: NotRequired[bool]
+    glow: NotRequired[bool]
 
 
 def generate_id(
@@ -18,20 +68,26 @@ def generate_id(
     )
 
 
-def expand_filament_data(manufacturer: str, data: dict) -> Iterator[dict]:
+def expand_filament_data(manufacturer: str, data: Filament) -> Iterator[dict]:
     """Expands the given filament data by generating multiple filament objects based on the weights, diameters, and colors."""
-    name: str = data["name"]
-    material: str = data["material"]
-    density: float = data.get("density", None)
-    weights: list[dict] = data["weights"]
-    diameters: list[float] = data["diameters"]
-    colors: list[dict] = data["colors"]
-    extruder_temp: int = data.get("extruder_temp", None)
-    bed_temp: int = data.get("bed_temp", None)
+    name = data["name"]
+    material = data["material"]
+    density = data["density"]
+    weights = data["weights"]
+    diameters = data["diameters"]
+    colors = data["colors"]
+    extruder_temp = data.get("extruder_temp", None)
+    bed_temp = data.get("bed_temp", None)
+    finish = data.get("finish", None)
+    multi_color_direction = data.get("multi_color_direction", None)
+    pattern = data.get("pattern", None)
+    translucent = data.get("translucent", False)
+    glow = data.get("glow", False)
 
     for weight_obj in weights:
         weight = weight_obj["weight"]
         spool_weight = weight_obj.get("spool_weight", None)
+        spool_type = weight_obj.get("spool_type", None)
 
         for diameter in diameters:
             for color_obj in colors:
@@ -39,6 +95,12 @@ def expand_filament_data(manufacturer: str, data: dict) -> Iterator[dict]:
                 color_hex = color_obj["hex"]
 
                 formatted_name = name.format(color_name=color_name)
+
+                if isinstance(color_hex, list):
+                    if multi_color_direction is None:
+                        raise ValueError(
+                            f"Filament {formatted_name} by {manufacturer} has multiple colors but no multi_color_direction specified."
+                        )
 
                 yield {
                     "id": generate_id(
@@ -50,10 +112,16 @@ def expand_filament_data(manufacturer: str, data: dict) -> Iterator[dict]:
                     "density": density,
                     "weight": weight,
                     "spool_weight": spool_weight,
+                    "spool_type": spool_type,
                     "diameter": diameter,
                     "color_hex": color_hex,
                     "extruder_temp": extruder_temp,
                     "bed_temp": bed_temp,
+                    "finish": finish,
+                    "multi_color_direction": multi_color_direction,
+                    "pattern": pattern,
+                    "translucent": translucent,
+                    "glow": glow,
                 }
 
 
